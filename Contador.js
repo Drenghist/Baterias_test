9bec48230616f1c3a469876842139df4021f51a6
Drenghist <59576956+Drenghist@users.noreply.github.com>
2022-11-14 21:28:03 +0100

    Add files via upload

--- Contador.js
+++ Contador.js
@@ -17,7 +17,9 @@ function iniciar() {
   //Meto los disparadores para cuando deselecciono uno de los inputs no NFC
   document.getElementById("input4").addEventListener("blur",prelanzar, false);
   document.getElementByID("input5").addEventListener("blur",prelanzar,false);
-  document.getElementByID("input6").addEventListener("change",prelanzar,false);
+  document.getElementByID("input6").addEventListener("change",alert("cambio"),false);
+  document.getElementByID("input6").addEventListener("blur",alert("blur"),false);
+  document.getElementByID("input6").addEventListener("keyup",alert("keyup"),false);
 }
 
 /*
